34b87fc0e08305a6427e62c6d11a1642ecd95e7e
Emil Kantis <e.kantis@gmail.com>
2015-04-18 23:56:17 +0200

    Played around a bit to get my bearings. Still have no idea how to write F# in a structured sense

--- GitSemVer/VersionCalculator.fs
+++ GitSemVer/VersionCalculator.fs
@@ -1,6 +1,21 @@
 ﻿module VersionCalculator
 
+open System.Text.RegularExpressions
 open System.IO
+open LibGit2Sharp
+
+let releaseBranchRegex =
+    "^release-(?<version>\d\.\d\.\d)$"
+
+let CurrentBranchIsReleaseBranch(repo:Repository) =
+    let matches = Regex.Match(repo.Head.Name, releaseBranchRegex)
+    matches.Groups.Item("version")
+
+let GetLatestTag(repo:Repository) =
+    let tags = seq { for i in repo.Tags do yield i }
+    seq { for i in tags do yield i.Name}
 
 let CalculateVersion(path:string) =
-    let repo = new LibGit2Sharp.Repository(path)
+    let repo = new Repository(path)
+    CurrentBranchIsReleaseBranch repo
+    
