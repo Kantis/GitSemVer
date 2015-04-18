0653866e241a57627fb72ed11ca5c33028a040f1
Emil Kantis <e.kantis@gmail.com>
2015-04-19 00:16:21 +0200

    Added a test project

---
+++ GitSemVer.Test/CalculateVersionTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitSemVer.Test
+{
+    [TestClass]
+    public class CalculateVersionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
